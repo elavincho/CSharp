366ede0a65109cd6c9b58355be29e964f7bfcb96
Mario David González <elavincho@gmail.com>
2025-04-06 00:21:51 -0300

    Ejercicio Agregar Clase

---
+++ EjercicioAgregarClase/EjercicioAgregarClase/Persona.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAgregarClase
+{
+    internal class Persona
+    {
+        // Atributos
+        private string nombre;
+        private int edad;
+
+        // Constructor
+        public Persona(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+
+        // Métodos get
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        public int getEdad()
+        {
+            return edad;
+        }
+
+        // Métodos set
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void setEdad(int edad)
+        {
+            this.edad = edad;
+        }
+
+        // Encapsulamiento por Properties
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+    }
+}
---
+++ EjercicioAgregarClase/EjercicioAgregarClase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAgregarClase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio Agregar una Clase");
+            Console.WriteLine("Creación atributos, get y set. Encapsulamiento por Properties");
+        }
+    }
+}
