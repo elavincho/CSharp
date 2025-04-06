a778da3cc4517fc6d7b17d3cf094ec57476f86dc
Mario David González <elavincho@gmail.com>
2025-04-05 23:52:52 -0300

    Ejercicio Super Heroe

---
+++ SuperHeroe/SuperHeroe/SuperHeroe.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroe
+{
+    internal class SuperHeroe
+    {
+        // Atributos
+        string nombre;
+        int fuerza;
+        int resistencia;
+        int superpoderes;
+
+        // Constructor
+        public SuperHeroe(string nombre, int fuerza, int resistencia, int superpoderes)
+        {
+            this.nombre = nombre;
+            this.fuerza = fuerza;
+            this.resistencia = resistencia;
+            this.superpoderes = superpoderes;
+        }
+
+        //Geters y Seters - Propiedades con validación de rango
+
+        public string Nombre
+        {
+            get { return nombre; }
+            private set { nombre = value; }
+        }
+
+        public int Fuerza
+        {
+            get { return fuerza; }
+            private set { fuerza = validarNumero(value); }
+        }
+
+        public int Resistencia
+        {
+            get { return resistencia; }
+            private set { resistencia = validarNumero(value); }
+        }
+
+        public int Superpoderes
+        {
+            get { return superpoderes; }
+            private set { superpoderes = validarNumero(value); }
+        }
+
+        // Método para validar que los atributos numéricos estén dentro del rango (0 a 100)
+        private int validarNumero(int value)
+        {
+            if (value < 0)
+                return 0;
+            else if (value > 100)
+                return 100;
+            return value;
+        }
+
+        // Método para comparar los poderes de dos superhéroes
+        public string competir(SuperHeroe otroSuperheroe)
+        {
+            string resultado = "EMPATE";
+            int misPuntos = 0;
+            int susPuntos = 0;
+
+            if (this.Fuerza > otroSuperheroe.Fuerza)
+                misPuntos++;
+            else if (this.Fuerza < otroSuperheroe.Fuerza)
+                susPuntos++;
+
+            if (this.Resistencia > otroSuperheroe.Resistencia)
+                misPuntos++;
+            else if (this.Resistencia < otroSuperheroe.Resistencia)
+                susPuntos++;
+
+            if (this.Superpoderes > otroSuperheroe.Superpoderes)
+                misPuntos++;
+            else if (this.Superpoderes < otroSuperheroe.Superpoderes)
+                susPuntos++;
+
+            if (susPuntos >= 2)
+                resultado = "DERROTA";
+            else if (misPuntos >= 2)
+                resultado = "TRIUNFO";
+            return resultado;
+        }
+    }
+}
---
+++ SuperHeroe/SuperHeroe/Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroe
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            // Crear los superhéroes según el enunciado
+            SuperHeroe superHeroe1 = new SuperHeroe("Batman", 90, 70, -10);
+            SuperHeroe superHeroe2 = new SuperHeroe("Superman", 95, 60, 70);
+
+            SuperHeroe superHeroe3 = new SuperHeroe("IronMan", 80, 90, 50);
+            SuperHeroe superHeroe4 = new SuperHeroe("Hulk", 150, 95, 30);
+
+            // Jugar y mostrar resultados
+            string resultado1 = superHeroe1.competir(superHeroe2);
+            string resultado2 = superHeroe2.competir(superHeroe1);
+
+            string resultado3 = superHeroe3.competir(superHeroe4);
+            string resultado4 = superHeroe4.competir(superHeroe3);
+
+            string resultado5 = superHeroe4.competir(superHeroe4);
+
+            Console.WriteLine("Resultado de Batman vs. Superman: " + resultado1);
+            Console.WriteLine("");
+            Console.WriteLine("Resultado de Superman vs. Batman: " + resultado2);
+            Console.WriteLine("");
+            Console.WriteLine("Resultado de IronMan vs. Hulk: " + resultado3);
+            Console.WriteLine("");
+            Console.WriteLine("Resultado de Hulk vs. IronMan: " + resultado4);
+            Console.WriteLine("");
+            Console.WriteLine("Resultado de Hulk vs. Hulk: " + resultado5);
+        }
+    }
+}
