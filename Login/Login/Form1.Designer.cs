b2864ab99c1692b730eb68bc19f49b6b8f16d219
Mario David González <elavincho@gmail.com>
2025-04-15 20:33:39 -0300

    Ejercicio Login

---
+++ Login/Login/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Login
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.picFondo = new System.Windows.Forms.PictureBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.picLogin = new System.Windows.Forms.PictureBox();
+            this.picIFTS = new System.Windows.Forms.PictureBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblTitulo2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picFondo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picIFTS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picFondo
+            // 
+            this.picFondo.BackColor = System.Drawing.Color.RoyalBlue;
+            this.picFondo.Location = new System.Drawing.Point(-1, -2);
+            this.picFondo.Name = "picFondo";
+            this.picFondo.Size = new System.Drawing.Size(294, 457);
+            this.picFondo.TabIndex = 0;
+            this.picFondo.TabStop = false;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.BackColor = System.Drawing.Color.RoyalBlue;
+            this.lblLogin.Font = new System.Drawing.Font("Gadugi", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblLogin.Location = new System.Drawing.Point(33, 43);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(220, 77);
+            this.lblLogin.TabIndex = 1;
+            this.lblLogin.Text = "LOGIN";
+            // 
+            // picLogin
+            // 
+            this.picLogin.BackColor = System.Drawing.Color.RoyalBlue;
+            this.picLogin.Image = ((System.Drawing.Image)(resources.GetObject("picLogin.Image")));
+            this.picLogin.Location = new System.Drawing.Point(46, 183);
+            this.picLogin.Name = "picLogin";
+            this.picLogin.Size = new System.Drawing.Size(191, 168);
+            this.picLogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogin.TabIndex = 2;
+            this.picLogin.TabStop = false;
+            // 
+            // picIFTS
+            // 
+            this.picIFTS.Image = ((System.Drawing.Image)(resources.GetObject("picIFTS.Image")));
+            this.picIFTS.Location = new System.Drawing.Point(632, 29);
+            this.picIFTS.Name = "picIFTS";
+            this.picIFTS.Size = new System.Drawing.Size(137, 122);
+            this.picIFTS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picIFTS.TabIndex = 3;
+            this.picIFTS.TabStop = false;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblUsuario.Location = new System.Drawing.Point(328, 183);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(82, 25);
+            this.lblUsuario.TabIndex = 4;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.AutoSize = true;
+            this.lblContrasena.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContrasena.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblContrasena.Location = new System.Drawing.Point(328, 277);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(117, 25);
+            this.lblContrasena.TabIndex = 4;
+            this.lblContrasena.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtUsuario.Location = new System.Drawing.Point(333, 211);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(302, 35);
+            this.txtUsuario.TabIndex = 5;
+            this.txtUsuario.Text = "Ingrese su Usuario";
+            this.txtUsuario.Enter += new System.EventHandler(this.txtUsuario_Enter);
+            this.txtUsuario.Leave += new System.EventHandler(this.txtUsuario_Leave);
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContrasena.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtContrasena.Location = new System.Drawing.Point(333, 305);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(302, 35);
+            this.txtContrasena.TabIndex = 5;
+            this.txtContrasena.Text = "Ingrese su Contraseña";
+            this.txtContrasena.Enter += new System.EventHandler(this.txtContrasena_Enter);
+            this.txtContrasena.Leave += new System.EventHandler(this.txtContrasena_Leave);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIngresar.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnIngresar.Location = new System.Drawing.Point(384, 374);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(205, 36);
+            this.btnIngresar.TabIndex = 6;
+            this.btnIngresar.Text = "INGRESAR";
+            this.btnIngresar.UseVisualStyleBackColor = false;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Gadugi", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblTitulo.Location = new System.Drawing.Point(317, 61);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(309, 28);
+            this.lblTitulo.TabIndex = 7;
+            this.lblTitulo.Text = "INSTITUTO DE FORMACIÓN";
+            // 
+            // lblTitulo2
+            // 
+            this.lblTitulo2.AutoSize = true;
+            this.lblTitulo2.Font = new System.Drawing.Font("Gadugi", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblTitulo2.Location = new System.Drawing.Point(341, 89);
+            this.lblTitulo2.Name = "lblTitulo2";
+            this.lblTitulo2.Size = new System.Drawing.Size(248, 31);
+            this.lblTitulo2.TabIndex = 7;
+            this.lblTitulo2.Text = "TÉCNICA SUPERIOR";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitulo2);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblContrasena);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.picIFTS);
+            this.Controls.Add(this.picLogin);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.picFondo);
+            this.Name = "frmLogin";
+            this.Text = "LOGIN";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picFondo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picIFTS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picFondo;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.PictureBox picLogin;
+        private System.Windows.Forms.PictureBox picIFTS;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContrasena;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblTitulo2;
+    }
+}
+
---
+++ Login/Login/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void picIFTS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            /* Este evento se ejecuta cuando llega al foco */
+
+            if(txtUsuario.Text == "Ingrese su Usuario")
+            {
+                txtUsuario.Text = "";
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            /* Este evento se ejecuta cuando se va el foco*/
+
+            if(txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "Ingrese su Usuario";
+            }
+        }
+
+        private void txtContrasena_Enter(object sender, EventArgs e)
+        {
+            /* Este evento se ejecuta cuando llega al foco */
+
+            if (txtContrasena.Text == "Ingrese su Contraseña")
+            {
+                txtContrasena.Text = "";
+                txtContrasena.UseSystemPasswordChar = true;
+            }
+
+        }
+
+        private void txtContrasena_Leave(object sender, EventArgs e)
+        {
+            /* Este evento se ejecuta cuando se va el foco*/
+
+            if (txtContrasena.Text == "")
+            {
+                txtContrasena.Text = "Ingrese su Contraseña";
+                txtContrasena.UseSystemPasswordChar = false;
+            }
+        }
+    }
+}
