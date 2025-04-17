f42cb749a249242964290d3c9683faf939ccd215
Mario David González <elavincho@gmail.com>
2025-04-16 23:45:05 -0300

    Ejercicio Grilla

---
+++ Grilla/Grilla/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Grilla
+{
+    partial class frmGrilla
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblCodigoProducto = new System.Windows.Forms.Label();
+            this.lblNombreProducto = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtCodigoProducto = new System.Windows.Forms.TextBox();
+            this.txtNombreProducto = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.dtgvDatos = new System.Windows.Forms.DataGridView();
+            this.codigoProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombreProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Gadugi", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblTitulo.Location = new System.Drawing.Point(271, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(233, 77);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "GRILLA";
+            // 
+            // lblCodigoProducto
+            // 
+            this.lblCodigoProducto.AutoSize = true;
+            this.lblCodigoProducto.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigoProducto.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblCodigoProducto.Location = new System.Drawing.Point(52, 126);
+            this.lblCodigoProducto.Name = "lblCodigoProducto";
+            this.lblCodigoProducto.Size = new System.Drawing.Size(168, 25);
+            this.lblCodigoProducto.TabIndex = 1;
+            this.lblCodigoProducto.Text = "Código Producto";
+            // 
+            // lblNombreProducto
+            // 
+            this.lblNombreProducto.AutoSize = true;
+            this.lblNombreProducto.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreProducto.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblNombreProducto.Location = new System.Drawing.Point(42, 194);
+            this.lblNombreProducto.Name = "lblNombreProducto";
+            this.lblNombreProducto.Size = new System.Drawing.Size(178, 25);
+            this.lblNombreProducto.TabIndex = 1;
+            this.lblNombreProducto.Text = "Nombre Producto";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblCantidad.Location = new System.Drawing.Point(115, 252);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(95, 25);
+            this.lblCantidad.TabIndex = 1;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // txtCodigoProducto
+            // 
+            this.txtCodigoProducto.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigoProducto.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtCodigoProducto.Location = new System.Drawing.Point(275, 126);
+            this.txtCodigoProducto.Name = "txtCodigoProducto";
+            this.txtCodigoProducto.Size = new System.Drawing.Size(270, 35);
+            this.txtCodigoProducto.TabIndex = 2;
+            // 
+            // txtNombreProducto
+            // 
+            this.txtNombreProducto.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreProducto.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtNombreProducto.Location = new System.Drawing.Point(275, 184);
+            this.txtNombreProducto.Name = "txtNombreProducto";
+            this.txtNombreProducto.Size = new System.Drawing.Size(270, 35);
+            this.txtNombreProducto.TabIndex = 2;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidad.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtCantidad.Location = new System.Drawing.Point(275, 242);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(270, 35);
+            this.txtCantidad.TabIndex = 2;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnBorrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBorrar.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnBorrar.Location = new System.Drawing.Point(625, 242);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(134, 35);
+            this.btnBorrar.TabIndex = 3;
+            this.btnBorrar.Text = "BORRAR";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnCargar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCargar.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCargar.Location = new System.Drawing.Point(625, 126);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(134, 35);
+            this.btnCargar.TabIndex = 3;
+            this.btnCargar.Text = "CARGAR";
+            this.btnCargar.UseVisualStyleBackColor = false;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // dtgvDatos
+            // 
+            this.dtgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.codigoProducto,
+            this.nombreProducto,
+            this.cantidad});
+            this.dtgvDatos.Location = new System.Drawing.Point(93, 331);
+            this.dtgvDatos.Name = "dtgvDatos";
+            this.dtgvDatos.ReadOnly = true;
+            this.dtgvDatos.Size = new System.Drawing.Size(644, 214);
+            this.dtgvDatos.TabIndex = 4;
+            this.dtgvDatos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgvDatos_CellClick);
+            this.dtgvDatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // codigoProducto
+            // 
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.codigoProducto.DefaultCellStyle = dataGridViewCellStyle10;
+            this.codigoProducto.HeaderText = "Código Producto";
+            this.codigoProducto.Name = "codigoProducto";
+            this.codigoProducto.ReadOnly = true;
+            this.codigoProducto.Width = 200;
+            // 
+            // nombreProducto
+            // 
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.nombreProducto.DefaultCellStyle = dataGridViewCellStyle11;
+            this.nombreProducto.HeaderText = "Nombre Producto";
+            this.nombreProducto.Name = "nombreProducto";
+            this.nombreProducto.ReadOnly = true;
+            this.nombreProducto.Width = 200;
+            // 
+            // cantidad
+            // 
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.cantidad.DefaultCellStyle = dataGridViewCellStyle12;
+            this.cantidad.HeaderText = "Cantidad";
+            this.cantidad.Name = "cantidad";
+            this.cantidad.ReadOnly = true;
+            this.cantidad.Width = 200;
+            // 
+            // frmGrilla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(800, 557);
+            this.Controls.Add(this.dtgvDatos);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtNombreProducto);
+            this.Controls.Add(this.txtCodigoProducto);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblNombreProducto);
+            this.Controls.Add(this.lblCodigoProducto);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmGrilla";
+            this.Text = "GRILLA";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblCodigoProducto;
+        private System.Windows.Forms.Label lblNombreProducto;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.TextBox txtCodigoProducto;
+        private System.Windows.Forms.TextBox txtNombreProducto;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.DataGridView dtgvDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigoProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombreProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidad;
+    }
+}
+
---
+++ Grilla/Grilla/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grilla
+{
+    public partial class frmGrilla : Form
+    {
+        // Variable que se usa para cualquier procedimiento de este formulario
+        int nro; 
+
+        public frmGrilla()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            // Agregar un renglon para la escritura
+            int renglon = dtgvDatos.Rows.Add();
+
+            // Colocamos los datos en las columnas de esa fila
+            dtgvDatos.Rows[renglon].Cells[0].Value = txtCodigoProducto.Text;
+            dtgvDatos.Rows[renglon].Cells[1].Value = txtNombreProducto.Text;
+            dtgvDatos.Rows[renglon].Cells[2].Value = txtCantidad.Text;
+
+            // Blanqueo de los textBox
+            txtCodigoProducto.Text = "";
+            txtNombreProducto.Text = "";
+            txtCantidad.Text = "";
+
+            // El foco se instala en el objeto
+            txtCodigoProducto.Focus();
+        }
+
+        private void dtgvDatos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Muestra la posicion de la fila seleccionada
+            nro = e.RowIndex;
+
+            if(nro != -1)
+            {
+                MessageBox.Show((string)dtgvDatos.Rows[nro].Cells[1].Value);
+            }
+            else
+            {
+                MessageBox.Show("¡Seleccionó el Encabezado!");
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            dtgvDatos.Rows.RemoveAt(nro);
+        }
+    }
+}
